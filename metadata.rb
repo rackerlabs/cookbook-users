214af3ddf7d32bb2ee57a6786b31b9c3486c2986
Shawn Ashlee <shawnashlee@yahoo.com>
2016-03-02 12:04:06 -0800

    remove chef-solo-search

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,4 @@ maintainer_email 'chef@lists.rackspace.com'
 license 'Apache 2.0'
 description 'Installs/Configures users'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '1.0.2'
-
-depends 'chef-solo-search'
+version '1.0.3'
